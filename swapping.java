43a8e77bcdf7c3b611934b880291ac6c3cb66479
Ankita pisal <82705895+AnkitaPisal1510@users.noreply.github.com>
2025-09-02 22:37:57 -0700

    Add files via upload

---
+++ swapping.java
@@ -0,0 +1,14 @@
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      class swap{
+	public static void main (String [] arg){
+
+	int a=10;
+	int b=15;
+	System.out.print("before swapping values of a ="+a+"and b="+b);
+	int temp;
+	temp = a;
+	a=b;
+	b=temp;
+	System.out.println("swapped values of a ="+a+"and b= "+b);
+	
+  }
+}                                                                                                                                                                                                       
